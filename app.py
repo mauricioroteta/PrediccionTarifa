d7386d639a454b52cbc982aa64acbb290e91b2f9
Mauricio Roteta <mauricioroteta@gmail.com>
2024-03-06 14:17:44 -0300

    primer commit

---
+++ app.py
@@ -0,0 +1,14 @@
+import streamlit as st 
+
+#Definir el título
+st.title('Mi primer app')
+
+st.write("Esta aplicación tiene el objetivo de elevar cualquier número al cuadrado:")
+
+x = st.number_input('Introduzca un número:')
+
+st.write('El número al cuadrado es:', x**2)
+
+
+
+var1 = st.sidebar.slider('Hola', 13, 78, 53)
