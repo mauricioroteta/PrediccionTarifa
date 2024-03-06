c16845191fa83df09a229a4d3e361ba4d374f2ee
Mauricio Roteta <mauricioroteta@gmail.com>
2024-03-06 16:22:38 -0300

    Base final

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
 import streamlit as st 
 
+st.markdown("""
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+""", unsafe_allow_html=True)
+
 #Definir el título
 st.title('Mi primer app')
 
@@ -8,7 +12,6 @@ st.write("Esta aplicación tiene el objetivo de elevar cualquier número al cuad
 x = st.number_input('Introduzca un número:')
 
 st.write('El número al cuadrado es:', x**2)
-
 
 def card():
     return """
